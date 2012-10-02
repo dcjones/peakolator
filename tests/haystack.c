d2a2a93bb634512d0b50f2c23bb8703c48bb4ae0
Daniel Jones <dcjones@cs.washington.edu>
2012-10-02 10:32:59 -0700

    Fix the haystack test.

--- tests/haystack.c
+++ tests/haystack.c
@@ -39,6 +39,7 @@ int main()
 
     interval_t* out;
     size_t count = peakolate(vec, f, g, 1, 10000, -INFINITY, 0, &out);
+    sort_intervals_des_density(out, count);
 
     if (count == 0) {
         fprintf(stderr, "No high density intervals were found.\n");
